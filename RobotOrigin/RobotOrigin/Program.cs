39f2f99bfa7f2f47ad12083d31d9cee932c32acf
Mellynkor <ricejr2@etsu.edu>
2022-06-03 14:58:34 -0400

    RobotOrigin
    LC to determine if a robot given a series of movement instructions in string form returns to its original 0,0 position

---
+++ RobotOrigin/RobotOrigin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotOrigin
+{
+    internal class Program
+    {
+        static string moves = "UDDU";
+        static void Main(string[] args)
+        {
+            JudgeCircle(moves);
+            bool JudgeCircle(string moves)
+            {
+
+                int[] position = new int[2];
+
+                for (int i = 0; i < moves.Length; i++)
+                {
+                    if (moves[i].Equals('U'))
+                    {
+                        position[0] += 1;
+                    }
+                    if (moves[i].Equals('D'))
+                    {
+                        position[0] -= 1;
+                    }
+                    if (moves[i].Equals('R'))
+                    {
+                        position[1] += 1;
+                    }
+                    if (moves[i].Equals('L'))
+                    {
+                        position[1] -= 1;
+                    }    
+                }
+                if (position[0] == 0 && position[1] == 0)
+                {
+                    Console.WriteLine("T");
+                    Console.ReadKey();
+                    return true;
+                    
+                }
+                else
+                    Console.WriteLine("F");
+                    Console.ReadKey();
+                    return false;
+                
+            }
+
+        }
+    }
+}
