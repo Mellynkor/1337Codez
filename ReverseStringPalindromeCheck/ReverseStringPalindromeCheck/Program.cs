72aae5d265f73986fcccf9bcac326712dac33b73
Mellynkor <ricejr2@etsu.edu>
2022-10-20 15:06:29 -0400

    ReverseString with additional logic to check if string was palindromic

---
+++ ReverseStringPalindromeCheck/ReverseStringPalindromeCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input a string please");
+string S = Console.ReadLine();
+string iS = "";
+for (int i = S.Length; i > 0; i--)
+{
+    iS += S[i - 1];
+
+}
+Console.WriteLine(iS);
+if (iS.ToLower().Equals(S.ToLower()))
+{
+    Console.WriteLine("The input string was a palindrome");
+}
+else
+{
+    Console.WriteLine("The input string was not a palindrome");
+}
+Console.ReadKey();
