0383fd21d9dbb6d6763fe87b451b444dca67163c
Mellynkor <ricejr2@etsu.edu>
2022-10-20 22:34:56 -0400

    MiniMax Sum
    given an array of 5 integers, sequentially calculate the minimum and maximum sum  of each set of exactly four integers in the array

---
+++ Mini-MaxSum/Mini-MaxSum/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long min = 0;
+        long max = 0;
+        long sum = 0;
+        int j = 0;
+
+        do
+        {
+            sum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                sum += arr[i];
+            }
+            sum -= arr[j];
+            if (j == 0)
+            {
+                min = sum;
+                max = sum;
+            }
+            if (sum < min)
+            {
+                min = sum;
+            }
+            if (sum > max)
+            {
+                max = sum;
+            }
+            j++;
+        } while (j < arr.Count);
+
+        Console.Write(min + " " + max);
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
