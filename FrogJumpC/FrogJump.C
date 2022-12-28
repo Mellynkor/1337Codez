c3b66d2ed7d9c5b95886658b4b174f83edc6e314
Mellynkor <ricejr2@etsu.edu>
2022-12-28 13:03:33 -0500

    Completed medium PQ leetcode
    Find index pairs such that P Index < Q Index && Q[value] < P[value]
    
    Required inverted for looping. 100% correctness, bad performance in edge cases of large data sets. O(n**2) complexity (doubling on each additional input ((because 2 loops)))

--- FrogJumpC/FrogJump.C
+++ FrogJumpC/FrogJump.C
@@ -1,4 +1,5 @@
 int solution(int X, int Y, int D) {
+
     for(int i = 1; X<Y; i++)
     {
         X+=D;
@@ -9,3 +10,39 @@ int solution(int X, int Y, int D) {
     }
     return 0;
 }
+
+10 20 5
+
+///
+My mother told  me
+Son
+You better have faith
+Cause its the one thing
+Thatll help you escape
+To a higher place
+
+Sweet nightmares
+Are all that's left
+Whispered words between life and death's
+Embrace
+Faceless in a shadows
+Guise
+Striving toward our demise
+School shooters
+Looters and riots
+Met with
+Silence
+Media lies and sensationalizes 
+The sensations of hatred
+And violence
+
+Every fuckin' day
+It's a new fuckin' thing
+Eyes glazed over glued to screens
+
+I'm sick I'm tired
+Of all this shit
+If you feel the same
+Then mosh with it
+
+They say sweet dreams are made of this
---
+++ MediumPQ/HardPQ/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+//P INDEX # must be LESS THAN Q INDEX #
+//Q[INDEX VALUE] MUST BE LESS THAN P[INDEX VALUE]
+
+//EX. 1,2  (P=1, < Q=2)  && P[6] > Q[3] 
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int[] A = new int[] { -1, 6, 3, 4, 7, 4 };
+        solution(A);
+    }
+    static int solution(int[] A)
+    {
+
+        var inversions = 0;
+        var pindex = 0;
+        var qindex = 0;
+        var qvalue = 0;
+        var pvalue = 0;
+
+        //Conditions to check for truths
+        // pvalue = A[1];
+        //pindex = 6;
+
+        //pvalue = A[1]; // 6//
+        //pindex = 1; // 1
+
+        //EX TRUTH pindex1 < qindex2 && (qvalue3 < 6pvalue,)
+
+        //Positive loop for Ps
+        for (int i = 0; i < A.Length-1; i++)
+        {
+            pvalue = A[i];
+            pindex = i;
+
+            //Inverted Loop for Qs
+            for (int j = A.Length - 1; j > 0; j--)
+            {
+                qvalue = A[j];
+                qindex = j;
+                //Console.WriteLine(pvalue + " " + qvalue);
+
+                if (pindex < qindex && qvalue < pvalue)
+                {
+                    //Console.WriteLine(pindex + " " + qindex);
+                    inversions++;
+                }
+            }
+        }
+        if (inversions > 1000000000)
+        {
+            return -1;
+        }
+        else
+            return inversions;
+        // Console.ReadKey();
+    }
+}
+
+/*
+ * using System;
+using System.Collections;
+//EX. 1,2  (P=1, < Q=2)  && P[6] > Q[3] 
+class Solution
+{
+    public int solution(int[] A)
+    {
+
+        var inversions = 0;
+        var pindex = 0;
+        var qindex = 0;
+        var qvalue = 0;
+        var pvalue = 0;
+
+        //EX TRUTH pindex1 < qindex2 && (qvalue3 < 6pvalue,)
+
+        //Positive loop for Ps
+        for (int i = 0; i < A.Length-1; i++)
+        {
+            pvalue = A[i];
+            pindex = i;
+
+            //Inverted Loop for Qs
+            for (int j = A.Length - 1; j > 0; j--)
+            {
+                qvalue = A[j];
+                qindex = j;
+                //Console.WriteLine(pvalue + " " + qvalue);
+
+                if (pindex < qindex && qvalue < pvalue)
+                {
+                    //Console.WriteLine(pindex + " " + qindex);
+                    inversions++;
+                }
+            }
+        }
+        if (inversions > 1000000000)
+        {
+            return -1;
+        }
+        else
+        return inversions;
+       // Console.ReadKey();
+    }
+}
+
+ */
